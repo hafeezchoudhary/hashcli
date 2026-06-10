46e0b631f456b8b7826cf129faab8935cd4c65ac
Hafeez choudhary <choudharyhafeez2513@gmail.com>
2026-06-10 21:28:00 +0530

    Refactor hash generation using algorithm dictionary

--- core/hash_text.py
+++ core/hash_text.py
@@ -2,18 +2,6 @@ import hashlib
 
 def hash_text(text) :
 
-    # hashed_sha256 = hashlib.sha256(text.encode()).hexdigest() 
-    # hashed_md5 = hashlib.md5(text.encode()).hexdigest()
-    # hashed_sha1 = hashlib.sha1(text.encode()).hexdigest()
-    # hashed_sha512 = hashlib.sha512(text.encode()).hexdigest() 
-
-    # return {
-    #     "SHA256": hashed_sha256,
-    #     "MD5": hashed_md5,
-    #     "SHA1": hashed_sha1,
-    #     "SHA512": hashed_sha512
-    # } 
-
     algorithms = {
         "MD5": hashlib.md5,
         "SHA1": hashlib.sha1,
