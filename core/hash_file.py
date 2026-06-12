d59cb53e7e5d11a4d837ef0cef84ea12a73a3bf3
Hafeez choudhary <choudharyhafeez2513@gmail.com>
2026-06-12 18:06:37 +0530

    Refactor hashing logic into shared byte hashing function

--- core/hash_file.py
+++ core/hash_file.py
@@ -5,21 +5,5 @@ def hash_file(path) :
     with open(path, "rb") as file :
         data = file.read() 
 
-        # results = {}
-        # algorithms = {
-        #     "SHA1": hashlib.sha1,
-        #     "MD5": hashlib.md5,
-        #     "SHA256": hashlib.sha256,
-        #     "SHA512": hashlib.sha512,
-        # } 
-
-        # for algorithm_name, algorithm_function in algorithms.items() :
-        #     results[algorithm_name] = algorithm_function(data).hexdigest()
-
-        # return results
-
         return hash_bytes(data)
 
-if __name__ == "__main__" :
-    data = hash_file("tests/sample.txt")
-    print(data)
