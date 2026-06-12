72915e7b89bfb6482fea3a6aa05121bff7f796a2
Hafeez choudhary <choudharyhafeez2513@gmail.com>
2026-06-12 17:42:30 +0530

    Add file hashing support

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from core.hash_text import hash_text 
 from output.console_output import display_hash_table 
+from core.hash_file import hash_file
 
 text = input()
-result = hash_text(text) 
-output = display_hash_table(result) 
+text_result = hash_text(text) 
+file_result = hash_file("tests/sample.txt")
+output = display_hash_table(text_result)  
+output = display_hash_table(file_result)  
 
