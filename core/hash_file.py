9dd179bb9ac18eb9af6c2de32359ab2a4bdfc763
Hafeez choudhary <choudharyhafeez2513@gmail.com>
2026-06-12 18:00:08 +0530

    Add file hashing support

--- core/hash_file.py
+++ core/hash_file.py
@@ -1,24 +1,25 @@
 import hashlib
+from core.hash_bytes import hash_bytes
 
 def hash_file(path) :
     with open(path, "rb") as file :
         data = file.read() 
 
-        results = {}
-        algorithms = {
-            "SHA1": hashlib.sha1,
-            "MD5": hashlib.md5,
-            "SHA256": hashlib.sha256,
-            "SHA512": hashlib.sha512,
-        } 
+        # results = {}
+        # algorithms = {
+        #     "SHA1": hashlib.sha1,
+        #     "MD5": hashlib.md5,
+        #     "SHA256": hashlib.sha256,
+        #     "SHA512": hashlib.sha512,
+        # } 
 
-        for algorithm_name, algorithm_function in algorithms.items() :
-            results[algorithm_name] = algorithm_function(data).hexdigest()
+        # for algorithm_name, algorithm_function in algorithms.items() :
+        #     results[algorithm_name] = algorithm_function(data).hexdigest()
 
-        return results
+        # return results
 
-        file.close() 
+        return hash_bytes(data)
 
 if __name__ == "__main__" :
-    result = hash_file("tests/sample.txt")
-    print(result) 
+    data = hash_file("tests/sample.txt")
+    print(data)
