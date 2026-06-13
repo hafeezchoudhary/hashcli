2f0f280eab137fa0d294440594d6ecce9266e055
Hafeez choudhary <choudharyhafeez2513@gmail.com>
2026-06-13 16:04:50 +0530

    Implement hash verification support

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from core.hash_text import hash_text 
 from output.console_output import display_hash_table 
 from core.hash_file import hash_file
+from core.verify_hash import verify_hash
 
 text = input()
 text_result = hash_text(text) 
 file_result = hash_file("tests/sample.txt")
 display_hash_table(text_result)  
 display_hash_table(file_result)  
+verify_result = verify_hash(file_result, "64ec88ca00b268e5ba1a35678a1b5316d212f4f366b2477232534a8aeca37f3c")
+print(verify_result) 
 
 
