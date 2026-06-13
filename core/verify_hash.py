8f9582eca1af78a4303bbfbca39976e5dd96bf91
Hafeez choudhary <choudharyhafeez2513@gmail.com>
2026-06-13 15:56:48 +0530

    Add hash verification functionality

--- core/verify_hash.py
+++ core/verify_hash.py
@@ -1,4 +1,17 @@
 import hashlib
 
 def verify_hash(results, target_hash) :
-    pass
+    
+    for algorithm_name, hash_value in results.items() :
+
+        if hash_value == target_hash : 
+            return {
+                "match": True,
+                "algorithm": algorithm_name
+            }
+            break
+
+    return {
+        "match": False,
+        "algorithm": None
+    }
