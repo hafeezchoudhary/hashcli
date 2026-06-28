81cbc0fa9bc724f71aa85735918314ed9da62b4a
Hafeez choudhary <choudharyhafeez2513@gmail.com>
2026-06-28 18:29:22 +0530

    Add formatted verification output

---
+++ output/verification_output.py
@@ -0,0 +1,31 @@
+from rich.console import Console
+from rich.table import Table
+from rich.panel import Panel
+from rich import box
+from core.verify_hash import verify_hash
+
+def display_verification_result(verification_result) :
+
+    status = "VERIFIED" if verification_result["match"] else "NOT VERIFIED"
+    algorithm = verification_result["algorithm"] if verification_result["algorithm"] else "NOT FOUND" 
+
+
+    display_data = {
+        "Status": status,
+        "Algorithm": algorithm,
+    }
+
+    table = Table(show_header=True, header_style="bold", show_lines=False, box=box.SIMPLE_HEAD)
+    table.add_column("Property") 
+    table.add_column("Value") 
+
+
+    for property_name, value in display_data.items() :
+        table.add_row(property_name, value) 
+   
+        
+    console = Console()
+    console.print(table) 
+
+if __name__ == "__main__" :
+    verification_result = verify_hash()
